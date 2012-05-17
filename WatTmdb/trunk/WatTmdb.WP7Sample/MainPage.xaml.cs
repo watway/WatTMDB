5601f5aa6b11c6c40afc26a700bccb1d908ab298
watway_cp <watway@users.noreply.github.com>
2012-05-17 12:07:21 +0000

    Windows Phone version of the library included

---
+++ WatTmdb/trunk/WatTmdb.WP7Sample/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using WatTmdb.V3;
+
+namespace WatTmdb.WP7Sample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private TmdbConfiguration tmdbConfig;
+        private string ApiKey { get; set; }
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ApiKey = "apikey";
+            Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // load the config for the image paths
+            var api = new Tmdb(ApiKey, null);
+            api.GetConfiguration(null, result => tmdbConfig = result.Data);
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbSearch.Text != "")
+            {
+                Tmdb api = new Tmdb(ApiKey, null);
+                api.SearchMovie(tbSearch.Text, 1, null, result =>
+                    {
+                        var baseUrl = "";
+                        if (tmdbConfig != null)
+                            baseUrl = string.Format("{0}{1}", tmdbConfig.images.base_url, tmdbConfig.images.poster_sizes[0]);
+
+                        if (result.Data != null)
+                        {
+                            listbox.ItemsSource = (from r in result.Data.results
+                                                   select new Movie
+                                                   {
+                                                       Title = r.title,
+                                                       ThumbnailUrl = r.poster_path != null ? baseUrl + r.poster_path : null
+                                                   }).ToList();
+                        }
+                        else
+                            MessageBox.Show(result.Error.status_message, "Error", MessageBoxButton.OK);
+                    });
+            }
+        }
+    }
+}
--- WatTmdb/trunk/WatTmdb.WP7Sample/Properties/AssemblyInfo.cs
+++ WatTmdb/trunk/WatTmdb.WP7Sample/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WatTmdb")]
+[assembly: AssemblyTitle("WatTmdb.WP7Sample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Watway")]
-[assembly: AssemblyProduct("WatTmdb")]
-[assembly: AssemblyCopyright("Copyright © Watway 2012")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("WatTmdb.WP7Sample")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5b410834-16c6-4953-b6ed-f04c9e9d20a7")]
+[assembly: Guid("8ecb63fa-e56b-403a-af82-9d08fe786316")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +30,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]

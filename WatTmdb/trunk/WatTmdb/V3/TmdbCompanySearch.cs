cc7167e83190c83563ec6fdf2eb803410a8b627c
watway_cp <watway@users.noreply.github.com>
2012-07-17 10:32:46 +0000

    - Add Timeout property to optionally control the request timeout to TMDB - throw exceptions caught by the RestClient, eg internet unavailable - New methods added - SearchCompany, GetGenreList, GetGenreMovies, GetUpcomingMovies, GetMovieTranslations - add comment block to each of the methods - change all methods taking an optional Language parameter to have more control on when to pass the Language (this will break existing code) - store the raw XML response in ResponseContent property

---
+++ WatTmdb/trunk/WatTmdb/V3/TmdbCompanySearch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WatTmdb.V3
+{
+    public class CompanyResult
+    {
+        public int id { get; set; }
+        public string logo_path { get; set; }
+        public string name { get; set; }
+    }
+
+    public class TmdbCompanySearch
+    {
+        public int page { get; set; }
+        public List<CompanyResult> results { get; set; }
+        public int total_pages { get; set; }
+        public int total_results { get; set; }
+    }
+}

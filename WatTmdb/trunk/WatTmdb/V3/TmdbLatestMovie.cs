4644b6e25a2d3073b5ab3cbd8fcabb3001e5b15b
watway_cp <SND\watway_cp>
2012-08-02 11:19:22 +0000

    Adjust the structure of the TmdbLatestMovie to remove the use of "object" types, to match the TmdbMovie object (thanks Venom_LoD)

--- WatTmdb/trunk/WatTmdb/V3/TmdbLatestMovie.cs
+++ WatTmdb/trunk/WatTmdb/V3/TmdbLatestMovie.cs
@@ -8,10 +8,10 @@ namespace WatTmdb.V3
     public class TmdbLatestMovie
     {
         public bool adult { get; set; }
-        public object backdrop_path { get; set; }
-        public object belongs_to_collection { get; set; }
+        public string backdrop_path { get; set; }
+        public BelongsToCollection belongs_to_collection { get; set; }
         public int budget { get; set; }
-        public List<object> genres { get; set; }
+        public List<MovieGenre> genres { get; set; }
         public string homepage { get; set; }
         public int id { get; set; }
         public string imdb_id { get; set; }
@@ -19,12 +19,12 @@ namespace WatTmdb.V3
         public string overview { get; set; }
         public double popularity { get; set; }
         public string poster_path { get; set; }
-        public List<object> production_companies { get; set; }
-        public List<object> production_countries { get; set; }
-        public object release_date { get; set; }
-        public int revenue { get; set; }
+        public List<ProductionCompany> production_companies { get; set; }
+        public List<ProductionCountry> production_countries { get; set; }
+        public string release_date { get; set; }
+        public Int64 revenue { get; set; }
         public int runtime { get; set; }
-        public List<object> spoken_languages { get; set; }
+        public List<SpokenLanguage> spoken_languages { get; set; }
         public string tagline { get; set; }
         public string title { get; set; }
         public double vote_average { get; set; }

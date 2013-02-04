107142f39c1e9cddc9e103beccb7b3085ed48077
watway_cp <SND\watway_cp>
2013-02-04 22:19:40 +0000

    - add new properties to configuration response class - create common base object for certain movie response classes

--- WatTmdb/trunk/WatTmdb/V3/TmdbGenreMovies.cs
+++ WatTmdb/trunk/WatTmdb/V3/TmdbGenreMovies.cs
@@ -5,22 +5,7 @@ using System.Text;
 
 namespace WatTmdb.V3
 {
-    public class GenreMovie
-    {
-        public string backdrop_path { get; set; }
-        public int id { get; set; }
-        public string original_title { get; set; }
-        public string release_date { get; set; }
-        public string poster_path { get; set; }
-        public string title { get; set; }
-        public double vote_average { get; set; }
-        public int vote_count { get; set; }
-
-        public override string ToString()
-        {
-            return title;
-        }
-    }
+    public class GenreMovie : TmdbMovieBase { }
 
     public class TmdbGenreMovies : TmdbSearchResultBase<GenreMovie>
     {
